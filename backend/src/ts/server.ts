67e18fa9cf64af8c798554b395b8fcbdbe72e847
Vasyl1yyy <tanksgold967@gmail.com>
2025-02-01 00:24:30 +0200

    open-server

--- backend/src/ts/server.ts
+++ backend/src/ts/server.ts
@@ -1,5 +1,23 @@
+import "dotenv/config";
 import knex from "knex";
 
-export const db = knex({
+const db = knex({
   client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
 });
+
+async function connectionTest() {
+  try {
+    await db.raw("SELECT 1");
+    console.log("Database connection established");
+  } catch (error) {
+    console.error("Database connection failed");
+  }
+}
+
+connectionTest();
