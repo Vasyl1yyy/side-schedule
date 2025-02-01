9480e8fcbd736e80e7799a3c978aa886c0ccc1d9
Vasyl1yyy <tanksgold967@gmail.com>
2025-02-02 00:15:59 +0200

    function(addIncome,addSchedule,getIncome,getSchedule)

---
+++ backend/src/ts/knex.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import knex from "knex";
+
+export const db = knex({
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+});
